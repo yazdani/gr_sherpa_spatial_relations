e0cf195017f752f1bd97c35086411f585976dcaf
Yazdani <yazdani@cs.uni-bremen.de>
2014-01-30 18:26:31 +0100

    added new files and other stuff

--- src/build-test-world.lisp
+++ src/build-test-world.lisp
@@ -30,73 +30,100 @@
 (in-package :sherpa)
 
 (defvar *list* nil)
+(defvar *joint-list* nil)
+(defvar *joint-states-sub* nil)
 (defparameter *amount-of-victims* 1)
 
 (defun start-scenario ()
   (start-myros)
   (start-bullet-with-robot)
-  (execute-trajectory))
-  ;(end-myros))
+  (execute-trajectory)
+  (end-myros))
 
-;; launch bullet_reasoning-files
+
+;; ;; (defun init-joint-states ()
+;; ;; (setf *joint-states-sub*
+;; ;;       (roslisp:subscribe "/joint_states"
+;; ;;                          "sensor_msgs/JointState"
+;; ;;                          #'joint-states-cb)))
+;; (defun joint-states-cb (msg)
+;;   (roslisp:with-fields (name position velocity effort) msg
+;;     (setf
+;;      *joint-list*
+;;      (loop for i from 0 below (length name)
+;;            for n = (elt name i)
+;;            for p = (elt position i)
+;;            for v = (elt velocity i)
+;;            for e = (elt effort i)
+;;            collect (cons n (list p v e))))))
 
 (defun start-bullet-with-robot ()
 ;(crs:prolog `(btr:debug-window ?w))
-;(setf *list* nil)
-(let* ((quad-urdf (cl-urdf:parse-urdf (roslisp:get-param "quad1/robot_description")))
-       (pr2-urdf (cl-urdf:parse-urdf (roslisp:get-param "robot_description_lowres")))
-       ;(world )
-       )
-  (setf *list*
-        (car 
-         (crs::force-ll
-          (crs:prolog
-           `(and
-             (btr:clear-bullet-world)
-             (btr:bullet-world ?w)
-             (assert (btr:object ?w btr:static-plane floor ((0 0 0) (0 0 0 1))
-                             :normal (0 0 1) :constant 0))
-           ;;(assert (btr:object world btr:static-plane mountain ((0 0 0) (0 0 0 1))
-                         ;;    :normal (0 0 1) :constant 0))
-              (btr:debug-window ?w)
-           ;   (btr:robot ?rob  )
-              (assert (btr:object ?w btr:urdf quad ((1 1 1) (0 0 0 1)) :urdf ,quad-urdf))
-           ;   (btr:robot ?robot)
-              (assert (btr:object ?w btr:urdf pr2 ((0 0 0) (0 0 0 1)) :urdf ,pr2-urdf)) 
-              (btr:robot-arms-parking-joint-states ?joint-states)
-             (assert (btr:joint-state ?w pr2 ?joint-states))
-             (assert (btr:joint-state ?w pr2 (("torso_lift_joint" 0.33))))
-)))))))
+(setf *list* nil)
+ (let* ((quad-urdf (cl-urdf:parse-urdf (roslisp:get-param "quad1/robot_description")))
+        (pr2-urdf (cl-urdf:parse-urdf (roslisp:get-param "robot_description_lowres")))
+        (tree-urdf (cl-urdf:parse-urdf (roslisp:get-param "env/tree_description"))))
+   (setf *list*
+         (car 
+          (crs::force-ll
+           (crs:prolog
+            `(and
+              (btr:clear-bullet-world)
+              (btr:bullet-world ?w)
+              (assert (btr:object ?w btr:static-plane floor ((0 0 0) (0 0 0 1))
+                              :normal (0 0 1) :constant 0))
+            ;;(assert (btr:object world btr:static-plane mountain ((0 0 0) (0 0 0 1))
+                          ;;    :normal (0 0 1) :consta(nt 0))
+               (btr:debug-window ?w)
+            ;   (btr:robot ?rob  )
+               (assert (btr:object ?w btr:urdf quad ((1 1 1) (0 0 0 1)) :urdf ,quad-urdf))
+            ;   (btr:robot ?robot)
+               (assert (btr:object ?w btr:urdf pr2 ((0 0 0) (0 0 0 1)) :urdf ,pr2-urdf)) 
+               (assert (btr:object ?w btr:urdf tree ((6 -6 1) (0 0 0 1)) :urdf ,tree-urdf))
+            ;   (btr:robot-arms-parking-joint-states ?joint-states)
+            ;  (assert (btr:joint-state ?w pr2 ?joint-states))
+           ;   (assert (btr:joint-state ?w pr2 (("torso_lift_joint" 0.33))))
+ )))))))
 
 
-(defun execute-trajectory()
-  (crs:prolog
-   `(assert (btr:joint-state ?w ?robot (("r_shoulder_pan_joint" 0.0)("r_shoulder_lift_joint" -0.5) ("r_upper_arm_roll_joint" 0.0)("r_elbow_flex_joint" 0.0)("r_forearm_roll_joint" 0.0)("r_wrist_flex_joint" 0.0)("r_wrist_roll_joint" 1.5))))))
+ (defun execute-trajectory()
+   (crs:prolog
+    `(assert (btr:joint-state ?w ?robot (("r_shoulder_pan_joint" 0.0)("r_shoulder_lift_joint" -0.5) ("r_upper_arm_roll_joint" 0.0)("r_elbow_flex_joint" 0.0)("r_forearm_roll_joint" 0.0)("r_wrist_flex_joint" 0.0)("r_wrist_roll_joint" 1.5))))))
 
-;(defun execute-trajectory-with-desig (action-desig)
-; (sherpa-spatial-designators action-desig))
+;; ;(defun execute-trajectory-with-desig (action-desig)
+;; ; (sherpa-spatial-designators action-desig))
  
-; (crs:prolog
-  ; `(and 
-   ;  (assert (btr:joint-state ?w ?robot (;;PROLOG METHOD WITH AN ACTION-DESIG)))))))
+;; ; (crs:prolog
+;;   ; `(and 
+;;    ;  (assert (btr:joint-state ?w ?robot (;;PROLOG METHOD WITH AN ACTION-DESIG)))))))
 
-(defun reset-arms-parking ()
-  (crs:prolog `(and 
-                (btr:robot-arms-parking-joint-states ?joint-states)
-                       (assert (btr:joint-state ?w ?robot ?joint-states)))))
+;; (defun reset-arms-parking ()
+;;   (crs:prolog `(and 
+;;                 (btr:robot-arms-parking-joint-states ?joint-states)
+;;                        (assert (btr:joint-state ?w ?robot ?joint-states)))))
 
 
-(defun start-myros ()
-  (roslisp-utilities:startup-ros :anonymous nil))
+ (defun start-myros ()
+   (roslisp-utilities:startup-ros :anonymous nil))
 
-(defun end-myros ()
-  (roslisp-utilities:shutdown-ros))
+ (defun end-myros ()
+   (roslisp-utilities:shutdown-ros))
+
+;; (defun start-projection ()
+;;  ; (right-arm-joint)
+;; )
+
+;; (defun make-joint-desig (joint-id)
+;;     (make-designator 'object `((desig-props:joint-name ,joint-id))))
+
 
 ;(cpl-impl:def-top-level-cram-function right-arm-joint ()
-;  (cram-projection:with-projection-environment
-;      projection-process-modules::pr2-bullet-projection-environment
+ ; (cram-projection:with-projection-environment
+ ;     projection-process-modules::pr2-bullet-projection-environment
 ;))
 
+;(cpl-impl:def-cram-fuction fly-close-to-the-cylinder (object-type cylinder-obj)
+ ;(format t "Fly to cylinder~%")
 ;(defun make-trajectory-desig ()
 ; (cram-designators:make-designator 'action `((joints (list ("" "" "" "" "" "" "" ""))) (values (list; (0.0 0.0 0.0 0.0 0.0 0.0)))))
 ;)
@@ -166,4 +193,5 @@
   (execute-right-arm-trajectory (position-to-trajectory)))
 
 
+ 
 
